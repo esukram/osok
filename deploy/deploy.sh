29babebb1e2f0a7e062f7b59f2f81842e585f7cb
Markus Heimbach <markus@esukram.org>
2021-03-07 11:46:06 +0100

    Create own deployment directory
    Use a dedicated directory for deployment artifacts.
    Adapt deployment script to use new directory structure.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 set -eu
+set -o pipefail
+
+# remember current dir
+CUR_DIR=$(pwd)
+
+# evaluate current directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${SCRIPT_DIR}"
 
 # number of app variants
 VARIANT=${VARIANT:-1}
 
 # build config map with current code
 kubectl create --dry-run=client configmap app -n app-x \
-    --from-file=router.ts=router.ts \
-    --from-file=app.ts=app.ts \
-    --from-file=fibonacci.ts=fibonacci.ts \
-    --from-file=k8s.ts=k8s.ts \
+    --from-file=router.ts=../router.ts \
+    --from-file=app.ts=../app.ts \
+    --from-file=fibonacci.ts=../fibonacci.ts \
+    --from-file=k8s.ts=../k8s.ts \
     -o yaml > app-2-cf.yaml
 
 # add labels to configmap
@@ -25,3 +33,5 @@ for counter in $(seq 1 $VARIANT); do
   done
 done
 
+# go back to previous directory
+cd "${CUR_DIR}"
